74aa8d4223b8fa4fa64c3e3bd1609b692eca074f
fredricksson <bandefredrickson@gmail.com>
2021-08-11 12:42:08 +0200

    updated

--- producer/index.js
+++ producer/index.js
@@ -8,10 +8,28 @@ const stream = kafka.Producer.createWriteStream({
 }, {} , {topic: 'test'})
 
 function queueMEssages() {
-    const event = { category: 'DOG', noise: "bark"}
+    const category = getRandomAnimal()
+    const noise = getRandomNoise(category)
+    const event = { category, noise}
     const result = stream.write(eventType.toBuffer(event))
     console.log(result)
 }
+function getRandomAnimal() {
+    const categories = ['CAT', 'DOG'];
+    return categories[Math.floor(Math.random() * categories.length)];
+  }
+
+  function getRandomNoise(animal) {
+    if (animal === 'CAT') {
+      const noises = ['meow', 'purr'];
+      return noises[Math.floor(Math.random() * noises.length)];
+    } else if (animal === 'DOG') {
+      const noises = ['bark', 'woof'];
+      return noises[Math.floor(Math.random() * noises.length)];
+    } else {
+      return 'silence..';
+    }
+  }
 
 setInterval(() => {
     queueMEssages();
