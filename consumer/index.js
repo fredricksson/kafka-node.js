80fba2ccecfef7ab24ec79854255ea687c75b5ba
fredricksson <bandefredrickson@gmail.com>
2021-08-11 11:51:07 +0200

    first commit

--- consumer/index.js
+++ consumer/index.js
@@ -1,7 +1,7 @@
 console.log("consumer....")
 import kafka  from "node-rdkafka"
 
-const stream = kafka.KafkaConsumer({
+const consumer = kafka.KafkaConsumer({
     'group.id' : 'kafka',
     'metadata.broker.list': 'localhost:9092'
 }, {} )
